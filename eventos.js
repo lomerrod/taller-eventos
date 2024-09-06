4f5ae42080a056e97fe4ba8f3674835a8ffd7767
lomerrod <lomerrod@gmail.com>
2024-09-05 22:02:23 -0300

    alerta div

---
+++ eventos.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    
+    let div = document.getElementById("container");
+
+    div.addEventListener("click", function () {
+    alert('Hola! Soy el div');  
+    });
+
+});
